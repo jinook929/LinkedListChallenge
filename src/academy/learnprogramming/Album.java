28f643bb3fd468485b9eb1a25f5d22b00324aa79
jinook929 <jinook929@gmail.com>
2019-12-16 22:16:36 -0500

    Test Main

--- src/academy/learnprogramming/Album.java
+++ src/academy/learnprogramming/Album.java
@@ -32,4 +32,24 @@ public class Album {
         }
         return null;
     }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if(index >= 0 && index < this.songs.size()) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = findSong(title);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
 }
